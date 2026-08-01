752c0f3113c88daa0d792996060472b4ac9ac164
BHARATH RAJ <cybercat120@gmail.com>
2026-08-01 16:03:00 +0530

    Update entire project

---
+++ Java-script/bac.js
@@ -0,0 +1,43 @@
+// var a =10
+// var b = 20
+// console.log(a+b);
+
+// let a=20
+// console.log(a);
+
+// {
+//     var a = 10;
+//     console.log(a); 
+// }
+
+// const a = 10;
+// console.log(a);
+
+// var price = 200
+// var product = "Mobile"
+// var tax = 0.18
+// console.log(product)
+// var total = price + tax
+// console.log(total)
+
+// var fruitsname =  "apple"
+// var count = 10
+// var price = 28
+// var total = count * price
+// console.log(fruitsname,total)
+
+// var a = 10
+// var b = a++
+// console.log(a)
+
+// console.log(typeof("10"))
+
+// var a = true
+// console.log(typeof(a))
+
+
+// function abc(){
+//     console.log("hello")
+// }
+// abc()
+
